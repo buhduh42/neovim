1244efdb0dca7e4008599469a8af04a18b0e2fe0
Dale Owens <dale.owens42@gmail.com>
2025-01-20 16:29:34 -0800

    reminder to enable syntax highlighting for gdscript

--- lua/gdscript.lua
+++ lua/gdscript.lua
@@ -5,6 +5,9 @@ local types = {
   "BufRead",
   "BufWinEnter",
 }
+
+-- getting bored of this, still a bit rusty with nvim
+-- do something along the lines of :setfiletype gdscript
 vim.api.nvim_create_autocmd(types, {
   pattern = {"*.gd"},
   callback = function(_)
